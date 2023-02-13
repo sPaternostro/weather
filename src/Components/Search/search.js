2ba4b35007ee8fd6b52d4948066a217d501a4d11
Sebastian <sebastian.paternostro@gmail.com>
2023-02-13 02:12:27 -0300

    first commit

---
+++ src/Components/Search/search.js
@@ -0,0 +1,48 @@
+import { useState } from "react"
+import { AsyncPaginate } from "react-select-async-paginate"
+import { GEO_API_URL, geoApiOptions } from "../../api"
+
+
+const Search = ({ onSearchChange }) => {
+
+    const [search, setSearch] = useState(null)
+
+    const loadOptions = (inputValue) => {
+        return fetch(
+            `${GEO_API_URL}/cities?minPopulation=100000&namePrefix=${inputValue}`, geoApiOptions
+        )
+
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.latitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch(err => console.error(err));
+
+    }
+
+    const handleOnChange = (searchData) => {
+        setSearch(searchData)
+        onSearchChange(searchData)
+    }
+
+
+    return (
+        <AsyncPaginate
+            placeholder="Search for a city"
+            
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
+        />
+    )
+}
+
+export default Search
---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'e586553c8bmshe7604bebb106760p19fa8bjsn39001fbc5f55',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5"
+
+export const WEATHER_API_KEY = "27f45f20ddf86db7ee986e5fdae8db72"
